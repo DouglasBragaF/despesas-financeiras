b94603e7a23299ec4626a2513fec7032890887cd
Douglas França <douglasbragaf@gmail.com>
2024-07-29 11:31:26 -0300

    init project

---
+++ src/main/java/com/br/despesas_financeiras/DespesasFinanceirasApplication.java
@@ -0,0 +1,13 @@
+package com.br.despesas_financeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasFinanceirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasFinanceirasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/despesas_financeiras/DespesasFinanceirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.despesas_financeiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasFinanceirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
